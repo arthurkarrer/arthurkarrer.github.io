cddcd5a02bed24d571cfd3f6344e6550d2dd6eba
Arthur Karrer <arthurkarrer@users.noreply.github.com>
2022-02-02 22:19:22 -0300

    Updates

---
+++ precache-manifest.31804a92b5e603bb17a6f2453f91ccbc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd02161f6bd206f87c38ee4d978e7c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "082554ac887b090854bd",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "082554ac887b090854bd",
+    "url": "/static/js/main.cc6933fe.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
